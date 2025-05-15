e59ba8f6e7784a798f0d1ef4bd7d6634e0cabc90
UdyanDSuradkar <suradkarudyan18@gmail.com>
2025-05-15 18:20:41 +0530

    Save work before switching branches

--- src/constants/index.js
+++ src/constants/index.js
@@ -11,10 +11,10 @@ const navLinks = [
     name: "Skills",
     link: "#skills",
   },
-  {
-    name: "Testimonials",
-    link: "#testimonials",
-  },
+  // {
+  //   name: "Testimonials",
+  //   link: "#testimonials",
+  // },
 ];
 
 const words = [
@@ -30,7 +30,7 @@ const words = [
 
 const counterItems = [
   { value: 15, suffix: "+", label: "Years of Experience" },
-  { value: 200, suffix: "+", label: " LC Problems Solved" },
+  { value: 250, suffix: "+", label: " LC Problems Solved" },
   { value: 10, label: "Completed Projects" },
   { value: 90, suffix: "%", label: "Client Retention Rate" },
 ];
@@ -159,32 +159,32 @@ const expCards = [
       "Optimized web applications for maximum speed and scalability.",
     ],
   },
-  {
-    review:
-      "Adrian’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
-    imgPath: "/images/exp2.png",
-    logoPath: "/images/logo2.png",
-    title: "Full Stack Developer",
-    date: "June 2020 - December 2023",
-    responsibilities: [
-      "Led the development of Docker's web applications, focusing on scalability.",
-      "Worked with backend engineers to integrate APIs seamlessly with the frontend.",
-      "Contributed to open-source projects that were used with the Docker ecosystem.",
-    ],
-  },
-  {
-    review:
-      "Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
-    imgPath: "/images/exp3.png",
-    logoPath: "/images/logo3.png",
-    title: "React Native Developer",
-    date: "March 2019 - May 2020",
-    responsibilities: [
-      "Built cross-platform mobile apps using React Native, integrating with Appwrite's backend services.",
-      "Improved app performance and user experience through code optimization and testing.",
-      "Coordinated with the product team to implement features based on feedback.",
-    ],
-  },
+  // {
+  //   review:
+  //     "Adrian’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
+  //   imgPath: "/images/exp2.png",
+  //   logoPath: "/images/logo2.png",
+  //   title: "Full Stack Developer",
+  //   date: "June 2020 - December 2023",
+  //   responsibilities: [
+  //     "Led the development of Docker's web applications, focusing on scalability.",
+  //     "Worked with backend engineers to integrate APIs seamlessly with the frontend.",
+  //     "Contributed to open-source projects that were used with the Docker ecosystem.",
+  //   ],
+  // },
+  // {
+  //   review:
+  //     "Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
+  //   imgPath: "/images/exp3.png",
+  //   logoPath: "/images/logo3.png",
+  //   title: "React Native Developer",
+  //   date: "March 2019 - May 2020",
+  //   responsibilities: [
+  //     "Built cross-platform mobile apps using React Native, integrating with Appwrite's backend services.",
+  //     "Improved app performance and user experience through code optimization and testing.",
+  //     "Coordinated with the product team to implement features based on feedback.",
+  //   ],
+  // },
 ];
 
 const expLogos = [
@@ -251,6 +251,7 @@ const socialImgs = [
   {
     name: "insta",
     imgPath: "/images/insta.png",
+    url: "https://www.instagram.com/_.udyan_one8?igsh=ajlsajhlcHlpeWp0",
   },
   {
     name: "fb",
